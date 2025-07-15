73f038914e579074677d07cb3d38869c03c67b94
Grace Shen <graceshen6439@gmaiil.com>
2025-07-15 13:13:16 -0400

    formatting of addbookview

--- shelfie/addBookView.swift
+++ shelfie/addBookView.swift
@@ -9,21 +9,51 @@ import SwiftUI
 
 struct addBookView: View {
     @State public var rating = 0.0
+    @State private var selectedStatus: Int? = nil
     
     var body: some View {
         VStack {
-            Text("Book Title:")
+                HStack {
+                    Spacer()
+                    Button("Back") {
+                        
+                    }
+                    .padding(.trailing, 25.0)
+                }
+            
+        
+            Text("Add a New Book")
                 .font(.title)
                 .fontWeight(.bold)
+                .padding(.top, 20.0)
             
+            Spacer()
+            
+            
+            Text("Book Title:")
             TextField("Type book name...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                 .padding()
                 .background(Color(.systemGroupedBackground))
                 .cornerRadius(15)
                 .padding()
             
+            
+              // nothing picked yet
+
+            Picker("Status", selection: $selectedStatus) {     // ← label always reads “Status”
+                Text("Want to read").tag(1 as Int?)
+                Text("In progress").tag(2 as Int?)
+                Text("Read").tag(3 as Int?)
+            }
+            
+            /*Picker(selection: .constant(1), label: Text("Status")) {
+                Text("Want to read").tag(1)
+                Text("In progress").tag(2)
+                Text("Read").tag(3)
+            }*/
+            
             HStack {
-                ForEach(0..<5) { i in
+                ForEach(0..<5) { i in //ill fix this later but it doesnt display 0 stars correctly
                     if rating >= Double(i) {
                         Text("star")
                     } else if rating.truncatingRemainder(dividingBy: 1) != 0 && rating >= Double(i) - 0.5 {
@@ -47,20 +77,21 @@ struct addBookView: View {
             }
             
             
-            Picker(selection: .constant(1), label: Text("Status")) {
-                Text("Want to read").tag(1)
-                Text("In progress").tag(2)
-                Text("Read").tag(3)
-            }
+
             
+        }
+        
+        VStack {
+            Spacer()
             Button("Add book") {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
             .font(.title)
             .fontWeight(.bold)
-            
         }
         .padding()
+        
+        
     }
 }
 
