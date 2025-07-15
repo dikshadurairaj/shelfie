a32b102c067fdb85b5bfba90962cad31a418175f
Harleen Monder <harleenmonder@gmail.com>
2025-07-15 17:13:42 +0200

    Added new SwiftUI file called writeReviewView

---
+++ shelfie/writeReviewView.swift
@@ -0,0 +1,18 @@
+//
+//  writeReviewView.swift
+//  shelfie
+//
+//  Created by Harleen Monder on 2025-07-15.
+//
+
+import SwiftUI
+
+struct writeReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    writeReviewView()
+}
