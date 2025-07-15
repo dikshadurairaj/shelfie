2bf90eb4c86fadd7cb62d48646f73c50be17ac8a
Harleen Monder <harleenmonder@gmail.com>
2025-07-15 18:00:40 +0200

    Added save button

--- shelfie/writeReviewView.swift
+++ shelfie/writeReviewView.swift
@@ -11,12 +11,29 @@ struct writeReviewView: View {
     @State private var bookName = ""
     
     var body: some View {
-        Text("Review for \(bookName)")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            Spacer()
+        VStack {
+            Text("Review for \(bookName)")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            TextField("Type here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .frame(width: 300, height: 25)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
+            
+            Button("Save") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.bordered)
+            .tint(.purple)
+            .padding(.top)
+                
+        }
         
-        TextField("Type here", text: $bookName)
+        .padding()
     }
 }
 
