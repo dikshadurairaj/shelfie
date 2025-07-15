31a2e96ab34ad02d9e6f3b4b12841daf087bcada
Diksha Durairaj <durairajdiksha@gmail.com>
2025-07-15 11:02:29 -0400

    Initial Commit

---
+++ shelfie/shelfieApp.swift
@@ -0,0 +1,17 @@
+//
+//  shelfieApp.swift
+//  shelfie
+//
+//  Created by Diksha Durairaj on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct shelfieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shelfieTests/shelfieTests.swift
@@ -0,0 +1,17 @@
+//
+//  shelfieTests.swift
+//  shelfieTests
+//
+//  Created by Diksha Durairaj on 7/15/25.
+//
+
+import Testing
+@testable import shelfie
+
+struct shelfieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
